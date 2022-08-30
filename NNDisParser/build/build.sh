23e9fe4586e1cfb1f5df147cb4e0d1db914307d0
Mat Sipahi <mohammad.sepahi@gmail.com>
2022-08-31 00:03:18 +0430

    redundants reverted

--- NNDisParser/build/build.sh
+++ NNDisParser/build/build.sh
@@ -1,2 +1,2 @@
-path=/home/ubuntu/NNDisParser-cp/
-cmake .. -DEIGEN3_DIR=${path}eigen -DN3L_DIR=${path}N3LDG -DMKL=TRUE -DCMAKE_BUILD_TYPE=Debug
+path=/home/yunan/workspace/discourseParser/
+cmake .. -DEIGEN3_DIR=${path}eigen -DN3L_DIR=${path}N3LDG -DMKL=TRUE
